455e59b70800d0560f787f7ecf5233b276bd8c3a
catzerovt <taniaalonzo.pe@gmail.com>
2026-05-11 19:59:22 -0500

    fix VideoBanner import

--- src/app/home/HomePage.jsx
+++ src/app/home/HomePage.jsx
@@ -4,7 +4,7 @@ import Navbar from "../../common/components/navbar/Navbar";
 import Hero from "../../common/components/hero/Hero";
 import Categories from "../../common/components/categories/Categories";
 import Products from "../../common/components/products/Products";
-import VideoBanner from "../../common/components/videoBanner/VideoBanner";
+import VideoBanner from "../../common/components/videobanner/VideoBanner";
 import Brands from "../../common/components/brands/Brands";
 import Services from "../../common/components/services/Services";
 import Blog from "../../common/components/blog/Blog";
